0774877cd05510ce8a0806351e10e3c5209bbb6d
teran210c <a242729@uach.mx>
2024-11-30 15:24:26 -0600

    added: HeroSection Css

---
+++ pages/heroSection.tsx
@@ -0,0 +1,10 @@
+// heroSection.tsx
+import styles from './heroSection.module.css';
+
+export default function HeroSection() {
+    return (
+        <div className={styles.heroSection}>
+            hola
+        </div>
+    );
+}
