ecbd45d36872f619c6662f1af5945b05f46d441c
teran210c <a242729@uach.mx>
2024-12-01 10:26:42 -0600

    colot boton modificado a esmeralda

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -54,7 +54,7 @@ export default function HeroSection() {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-[#22682b] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#549b5e] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-emerald-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#549b5e] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Consulta el catalogo
               </a>
